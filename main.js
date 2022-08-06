ffeb7a4095fb29795f1c28e3d4052485a70d737c
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-08-06 12:20:29 +0530

    Project - 107

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 ///https: //teachablemachine.withgoogle.com/models/GNSCC88AK/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/GNSCC88AK/model.json", modelReady);
+}
+
+function modelReady() {
+    classifier.classify(getResults);
+}
+
+function getResults(error, results) {
+    console.log("Got Results!!!");
+}
